3e203fcced105f73be8e23fc7600cedeb29401e9
jjunxi <jjunxi417@gmail.com>
2017-11-13 21:48:44 +0800

    get all

--- web/web-server/services/toto.service.js
+++ web/web-server/services/toto.service.js
@@ -11,7 +11,7 @@ var getTotos = function () {
             } else {
                 resolve(totos);
             }
-        }).sort({ number: -1 }).limit(50);
+        }).sort({ number: -1 });
     });
 };
 
