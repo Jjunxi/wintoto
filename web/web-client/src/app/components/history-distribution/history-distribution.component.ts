ba4b2a84ab946bc840d09ce06f803976d3ad0bef
jeff994 <hujianbo1981@gmai.com>
2017-12-04 15:27:52 +0800

    added different color for max and min winning number

--- web/web-client/src/app/components/history-distribution/history-distribution.component.ts
+++ web/web-client/src/app/components/history-distribution/history-distribution.component.ts
@@ -6,44 +6,6 @@ import Chart from 'chart.js';
 
 const X = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49'];
 
-
-function create_dict(X, value)
-{
-  var dict = new Array();
-  for(var i = 0; i < 49; i++)
-  {
-    dict[X[i]] = value[i]; 
-  }
-  //console.log(dict);
-  var items = Object.keys(dict).map(function(key) {
-      return [key, dict[key]];
-  });
-
-  // Sort the array based on the second element
-  items.sort(function(first, second) {
-      return second[1] - first[1];
-  });
-  console.log(items);
-  return dict; 
-}
-
-var dict_seven;
-var dict_additional; 
-var dict_six; 
-
-var Y_seven = [];
-var Y_additional = []; 
-var Y_six = []; 
-
-
-for(var i = 0; i < 50; i++)
-{
-  Y_seven[i] = 0 ; 
-  Y_additional[i] = 0; 
-  Y_six[i] = 0; 
-}
-
-
 const COLORS = {
   red: 'rgb(255, 99, 132)',
   orange: 'rgb(255, 159, 64)',
@@ -53,6 +15,15 @@ const COLORS = {
   purple: 'rgb(153, 102, 255)',
   grey: 'rgb(201, 203, 207)'
 };
+
+var dict_seven;
+var dict_additional; 
+var dict_six; 
+
+var Y_seven = [];
+var Y_additional = []; 
+var Y_six = []; 
+
 
 @Component({
   selector: 'app-history-distribution',
@@ -67,6 +38,7 @@ export class HistoryDistributionComponent implements OnInit {
 
   ngOnInit() {
     console.log("test");
+    this.initChartData();
     this.getTotos();
   }
 
@@ -90,19 +62,68 @@ export class HistoryDistributionComponent implements OnInit {
              }); 
          }); 
 
-        dict_seven      = create_dict(X, Y_seven);
-        dict_six        = create_dict(X, Y_six);
-        dict_additional = create_dict(X, Y_additional);
+        var Y_seven_color       = this.specifyColor(X, Y_seven, 7);
+        var Y_six_color         = this.specifyColor(X, Y_six, 6);
+        var Y_additional_color  = this.specifyColor(X, Y_additional, 1); 
 
-        this.drawBarChart($('#myChart'), Y_seven);   
-        this.drawBarChart($('#myChartAdditional'), Y_additional);   
-        this.drawBarChart($('#myChartSixNumber'), Y_six);   
+        this.drawBarChart($('#myChart'), Y_seven, Y_seven_color);   
+        this.drawBarChart($('#myChartAdditional'), Y_additional, Y_additional_color);   
+        this.drawBarChart($('#myChartSixNumber'), Y_six, Y_six_color);   
       })
       .catch(err => console.log(err));
   }
 
-  drawBarChart(ctx, datalist): void {
+  initChartData():void{
     var color = Chart.helpers.color;
+    for(var i = 0; i < 50; i++)
+    {
+      Y_seven[i] = 0 ; 
+      Y_additional[i] = 0; 
+      Y_six[i] = 0; 
+    }
+  }
+
+  createDict(X, value){
+    var dict = new Array();
+    for(var i = 0; i < 49; i++)
+    {
+      dict[X[i]] = value[i]; 
+    }
+    //console.log(dict);
+    var items = Object.keys(dict).map(function(key) {
+        return [key, dict[key]];
+    });
+
+    // Sort the array based on the second element
+    items.sort(function(first, second) {
+        return second[1] - first[1];
+    });
+
+    console.log("Itemds dict");
+    return items; 
+  }
+
+  specifyColor(X, Y, n){
+    var color = Chart.helpers.color;
+    var Y_color = []; 
+    var items = this.createDict(X, Y);
+    //console.log(items);
+    var count = 0; 
+    for (var key in items) {
+      //console.log("key is :" + items[count][0]);
+      var index = parseInt(items[count][0]) - 1;
+      if(count < n) 
+        Y_color[index] = color(COLORS.purple).alpha(0.5).rgbString();
+      else if(count >= 49-n) 
+        Y_color[index] = color(COLORS.grey).alpha(0.5).rgbString();
+      else 
+        Y_color[index] = color(COLORS.red).alpha(0.5).rgbString();
+      count++; 
+    }
+    return Y_color; 
+  }
+
+  drawBarChart(ctx, datalist, colorlist): void {
     console.log(ctx);
     var frequency = [];
 
@@ -114,7 +135,7 @@ export class HistoryDistributionComponent implements OnInit {
         datasets: [{
           label: 'No. Winning',
           data: datalist,
-          backgroundColor: color(COLORS.red).alpha(0.5).rgbString(),
+          backgroundColor: colorlist,
         }],
       },
       
